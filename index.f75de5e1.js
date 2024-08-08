d4994d2efae3613d29041034f9cb8f8c3918d0c0
voidmain69 <photoreznichenko@gmail.com>
2024-08-08 14:20:51 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    notification.classList.add("notification", type);
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    const titleElement = document.createElement("h2");
+    titleElement.classList.add("title");
+    titleElement.textContent = title;
+    const pElement = document.createElement("p");
+    pElement.innerText = description;
+    notification.appendChild(titleElement);
+    notification.appendChild(pElement);
+    document.body.appendChild(notification);
+    setTimeout(()=>{
+        document.body.removeChild(notification);
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
